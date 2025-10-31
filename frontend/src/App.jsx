9c25848150faceffa1580e7aab69169ae224320b
Nick-Mercandetti <nicholas.mercandetti@gmail.com>
2025-10-31 15:12:43 -0500

    Front end initialized

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Todo App</h1>
+      </header>
+      <main>
+        <p>Todo components will go here</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
+
